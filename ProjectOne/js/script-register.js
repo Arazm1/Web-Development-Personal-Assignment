71b137f3d4a837383abecca9913381a594a63471
Araz <arazm@metropolia.fi>
2025-11-09 20:00:11 +0200

    Add files via upload

---
+++ ProjectOne/js/script-register.js
@@ -0,0 +1,15 @@
+/*
+Test code
+const avatarInput = document.getElementById('avatar-input');
+const avatarPreview = document.getElementById('avatar-preview');
+
+avatarInput.addEventListener('change', () => {
+    const file = avatarInput.files[0];
+    if (file) {
+        avatarPreview.src = URL.createObjectURL(file);
+        avatarPreview.style.display = 'block';
+    } else {
+        avatarPreview.style.display = 'none';
+    }
+});
+*/
